b8c026795eb3cd8acd1ada54269631af3ee5b940
Molnár Sándor <molnar.sandor@wwdh.hu>
2018-11-13 11:54:58 +0100

    #1

--- src/widgets/ColumnListView.php
+++ src/widgets/ColumnListView.php
@@ -1,11 +1,15 @@
 <?php
-namespace circulon\widgets;
+
+namespace sanyisasha\yii2\columnlistview\widgets;
+
 use yii\helpers\Html;
 use yii\helpers\ArrayHelper;
+
 /**
  * @author SaSha <molnar.sandor.benjamin@gmail.com>
  */
 class ColumnListView extends \yii\widgets\ListView
 {
+
 
 }
